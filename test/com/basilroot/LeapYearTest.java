44a4fb07551370aff90755ee3d7b3a185682c5dd
Suraj Tekchandani <suraj.tekchandani@basilroot.com>
2020-05-11 12:42:40 +0530

    Teast modification

--- test/com/basilroot/LeapYearTest.java
+++ test/com/basilroot/LeapYearTest.java
@@ -1,17 +1,83 @@
 package com.basilroot;
+
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
-
-
 public class LeapYearTest {
-	
+
+	private static LeapYear leapYear;
+	@BeforeClass
+	public static void onceExecutedBeforeAll() {
+		leapYear = new LeapYear();
+	}
+
 	@Test
 	public void testIsLeap() {
-		LeapYear leapYear = new LeapYear();
 		assertTrue(leapYear.isLeap(2004));
-		assertFalse(leapYear.isLeap(2100));		
+	}
+
+	@Test
+	public void testIsNotLeapYear() {
+		assertFalse(leapYear.isLeap(2100));
+	}
+
+	@Test
+	public void test2000IsLeap() {
+		assertTrue(leapYear.isLeap(2000));
+	}
+
+	@Test
+	public void test1700IsLeap() {
+		assertFalse(leapYear.isLeap(1700));
+	}
+
+	@Test
+	public void test1800IsLeap() {
+		assertFalse(leapYear.isLeap(1800));
+	}
+
+	@Test
+	public void test1900IsLeap() {
+		assertFalse(leapYear.isLeap(1900));
+	}
+
+	@Test
+	public void test2100IsLeap() {
+		assertFalse(leapYear.isLeap(2100));
+	}
+
+	@Test
+	public void test2008IsLeap() {
+		assertTrue(leapYear.isLeap(2008));
+	}
+
+	@Test
+	public void test2012IsLeap() {
+		assertTrue(leapYear.isLeap(2012));
+	}
+
+	@Test
+	public void test2016IsLeap() {
+		assertTrue(leapYear.isLeap(2016));
+	}
+
+	@Test
+	public void test2017IsLeap() {
+		assertFalse(leapYear.isLeap(2017));
+	}
+
+	@Test
+	public void test2018IsLeap() {
+		assertFalse(leapYear.isLeap(2018));
+	}
+
+	@Test
+	public void test2019IsLeap() {
+		assertFalse(leapYear.isLeap(2019));
 	}
 
 }
